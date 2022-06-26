32ef2b1b4c92805bc2a8a5b016bcca1c684674fb
Yaseen Wakeel <chyaseen34@gmail.com>
2022-06-26 21:08:54 +0500

    image asset added

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,28 @@ class DiceeApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text("DiceeApp"),
+          backgroundColor: Colors.red,
+          title: Center(
+            child: Text("DiceeApp"),
+          ),
+        ),
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset('images/dice1.png'),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset('images/dice2.png'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
